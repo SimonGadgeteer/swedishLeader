0d448ab9870c7e7deee683658cabbd91e149c74e
Simon Schwarz <schwasi2@students.zhaw.ch>
2016-10-31 11:32:45 +0100

    changes: - changed to Python3 - implemented voting (random 0 / 1) - implemented gathering of node list - implemented self registering @ registry
    todo:
    - synchronize voting process
    - store key values
    - synchronize kv stores
    - include billing with Cyclops (see Lab07)

--- app.py
+++ app.py
@@ -3,35 +3,60 @@
 port = 8080
 host = ''
 
-import urllib2
-#probably no necessary --> remote kv store in use; import registry 
+import urllib
+# probably no necessary --> remote kv store in use; import registry
 import leader
-#to do import store
-#import config
+from random import randint
+
+
+# to do import store
+# import config
 
 
 def registerself():
-	try:
-		register = urllib2.Request('http://kvstore-python-kvstore.44fs.preview.openshiftapps.com/registry/'+ selfnodeName)
-		response = urllib2.urlopen(register)
-		registerResponse = response.read()
-		print("Node registered as "+registerResponse)
-	except Exception as e:
-		print("Error registering: ",e)
+    try:
+        response = urllib.urlopen(
+            'http://kvstore-python-kvstore.44fs.preview.openshiftapps.com/registry/touriste/1272');
+        registerResponse = response.read()
+        print("Node registered as " + registerResponse)
+    except Exception as e:
+        print("Error registering: ", e)
+
 
 def application(environ, start_response):
-		ctype = "text/plain"
-		status = "200 OK"
+    ctype = "text/plain"
+    status = "200 OK"
+
+    if environ['PATH_INFO'].startswith("/store"):
+        return store.application(environ, start_response)
+    elif environ['PATH_INFO'].startswith("/leader"):
+        return True
+        if environ['PATH_INFO'].startswith("/leader/vote"):
+            start_response(status, "('Content-Type', ctype)")
+            return vote()
+    else:
+        start_response("400", "('Content-Type', ctype)")
 
 
-		if environ['PATH_INFO'].startswith("/store"):
-			return store.application(environ, start_response)
-		else:
-			return leader.application(environ, start_response)
+def getNodeList():
+    try:
+        response = urllib.urlopen(
+            'http://kvstore-python-kvstore.44fs.preview.openshiftapps.com/registry/touriste');
+        registerResponse = response.read()
+        listOfAvailableNodes = registerResponse[13:].split(",")
+        print(listOfAvailableNodes)
+    except Exception as e:
+        print("Error registering: ", e)
+
+def vote():
+    return randint(0,1)
 
 if __name__ == '__main__':
-	from wsgiref.simple_server import make_server
-	httpd = make_server(host, port, application)
-	while True:
-			print("waiting")
-			httpd.handle_request()
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server(host, port, application)
+    registerself()
+    getNodeList()
+# while True:
+#    print("waiting")
+#    httpd.handle_request()
