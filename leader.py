70f1449896513fc5fdf249ddc4fe2ccac97c7dfa
Martin <martin@buergesoft.ch>
2016-11-07 09:27:17 +0100

    Fixing response decoding on /vote

--- leader.py
+++ leader.py
@@ -5,7 +5,7 @@ from random import randint
 def getNodeList():
     try:
         response = urllib.request.urlopen(
-            'http://isprot-registry.appspot.com/registry/touriste6')
+            'http://isprot-registry.appspot.com/registry/touriste8')
         registerResponse = response.read().decode('UTF-8')
 
         if registerResponse.startswith('Participants'):
@@ -33,7 +33,7 @@ def election(localhost, port):
                 if host.strip() != localhost + ':' + str(port):
 
                     response = urllib.request.urlopen('http://' + host.strip() + '/leader/vote')
-                    response = int(response.decode('UTF-8'))
+                    response = int(response.read().decode('UTF-8'))
                 else:
                     response = int(vote())
 
